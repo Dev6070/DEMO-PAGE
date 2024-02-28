dab5b9c1330d48de84321a548c4dc990e8232292
Dev6070 <devesh.6070@gmail.com>
2024-02-28 13:24:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div className="page">
+        <div className="container-1 d-flex flex-wrap flex-row">
+            <div className="img-container ">
+                <img src="./asserts/1.png" alt="img"/>
+            </div>
+            <div className="img-container-2 p-2 ">
+                <div className="heading p-3 pb-4">
+                    <img src="./asserts/logo.png"/>
+
+                </div>
+                <p className="bold-heading"><b>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.
+
+                </b></p>
+                <ul>
+                    <li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.
+                    </li>
+                    <li>C.R.1. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.
+                    </li>
+                </ul>
+<div className="img-container">
+<img src="./asserts/2.png"/>
+</div>
+                <p>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.
+
+                    INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
+            </div>
+        </div>
+        <div className="container-2 d-flex flex-column">
+  <p className="p-2">
+    INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.
+  </p>
+  <div className="image-container p-3 px-2 py-3">
+    <img src="./asserts/3.png" alt="img"/>
+  </div>
+  <div className="text-center mt-5 pb-3">Valves Pumps Pipes IoT Drives & Controllers Wires & Cables Solar Systems Motors</div>
+
+  <div className="container-3 text-center">
+    <b>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</b><br/>
+    CHEMICALS & PROCESS POWER WATER & WASTE WATER OILS & GAS PHARMA SUGARS & DISTILLERIES PAPER & PULP MARINE & DEFENCE METAL & MINING FOOD & BEVERAGE PETROCHEMICAL & REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE & RESIDENTIAL
+  </div>
+  <div className="footer p-3 pt-5"> 
+            <div className="d-flex flex-wrap flex-rown justify-content-around" style={{paddingBottom:'2%'}}>
+                <div><i className="fas fa-phone"></i>&nbsp;
+                    Toll Free 1800 200 1234</div>
+                <div> <i className="fab fa-facebook"></i>&nbsp; wwww.facebook.com/cripumps</div>
+                <div><i className="fas fa-globe"></i>&nbsp;www.crigroups.com</div>
+            </div>
+</div>
+
+      
+
+        </div>
+   </div>
+   
+   
+   </>
   );
 }
 
